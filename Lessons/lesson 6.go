afd9038761a34336ecb59a694699c1732e7186da
Samir Madjidov HPLaptop Humo <madjidovtop1@gmail.com>
2025-10-15 18:22:11 +0500

    Телефоная книга

--- Lessons/lesson 6.go
+++ Lessons/lesson 6.go
@@ -1,1 +1,19 @@
 package main
+
+import "fmt"
+
+func main() {
+	phonebook := make(map[string]string)
+	phonebook["Jan"] = "+9924875487636"
+	phonebook["Gam"] = "+9929056744553"
+	phonebook["Irina"] = "+992209004765"
+
+	var name string
+	fmt.Print("Кого ещем: ")
+	fmt.Scan(&name)
+	if phone, exists := phonebook[name]; exists {
+		fmt.Printf("Номер %s: %s\n", name, phone)
+	} else {
+		fmt.Printf("Контак %s не найден\n", name)
+	}
+}
